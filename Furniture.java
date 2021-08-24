391f59993ee0298a29beaa4de019186c047989bc
Naman <namansharmanaman1111@gmail.com>
2021-08-24 13:48:34 +0530

    Commit111

---
+++ Furniture.java
@@ -0,0 +1,6 @@
+public class Furniture {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
--- main.java
+++ main.java
@@ -1,5 +1,8 @@
-class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello Naman");
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Employee Code:101        Employee Name:Rohan Kumar     Basic Salary:Rs. 30000");
+        System.out.println("HRA:10%of Basic Salary   DA:10%of Basic Salary         TA:7% of Basic Salary");
+        System.out.println("PF:5%of Basic Salary     Department:Sales Department   Designation:Sales");
+
     }
 }
