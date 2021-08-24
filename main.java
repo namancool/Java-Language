a77666a26c118c6490c7ea56fda764c894906743
Naman <namansharmanaman1111@gmail.com>
2021-08-24 13:57:01 +0530

    commmit1

--- main.java
+++ main.java
@@ -1,4 +1,4 @@
-class Main {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Employee Code:101        Employee Name:Rohan Kumar     Basic Salary:Rs. 30000");
         System.out.println("HRA:10%of Basic Salary   DA:10%of Basic Salary         TA:7% of Basic Salary");
