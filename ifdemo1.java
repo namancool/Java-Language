5c3c1a3f9f86634dfe830bfd2715ec3107966a31
namancool <namansharmanaman1111@gmail.com>
2021-09-22 11:27:51 +0530

    Commit2

---
+++ ifdemo1.java
@@ -0,0 +1,8 @@
+public class ifdemo1 {
+    public static void main(String args[]) {
+        int age = 28;
+        if (age >= 18) {
+            System.out.println("Person is eligible to Vote");
+        }
+    }
+}
