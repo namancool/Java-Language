a2a1b8c68fa98a5d50949f9a2173c74788cc104e
namancool <namansharmanaman1111@gmail.com>
2021-09-22 12:02:16 +0530

    Commit123

--- ifdemo1.java
+++ ifdemo1.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class ifdemo1 {
     public static void main(String args[]) {
-        int age = 28;
+        // int age = 8;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Age : ");
+        int age = sc.nextInt();
         if (age >= 18) {
             System.out.println("Person is eligible to Vote");
+        } else {
+            System.out.println("Not eligible to vote");
         }
+
     }
 }
