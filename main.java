9846b2f5bb0dde9a040883458577608b0ab7703c
namancool <namansharmanaman1111@gmail.com>
2021-09-22 11:08:28 +0530

    Commit

--- main.java
+++ main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Employee Code:101        Employee Name:Rohan Kumar     Basic Salary:Rs. 30000");
-        System.out.println("HRA:10%of Basic Salary   DA:10%of Basic Salary         TA:7% of Basic Salary");
-        System.out.println("PF:5%of Basic Salary     Department:Sales Department   Designation:Sales");
+        System.out.println("NAMAN SHARMA");
 
     }
 }
